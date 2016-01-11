6abcfe8b7888652061ae27ef702057a4889f4454
mdymen <msn@dymenstein.com>
2016-01-11 20:04:40 -0200

    novas telas

---
+++ src/com/examplet/teste/Inicio.java
@@ -0,0 +1,77 @@
+package com.examplet.teste;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class Inicio extends AppCompatActivity {
+
+	   ListView listView; 
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		 // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+        
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Android List View", 
+                                         "Adapter implementation",
+                                         "Simple List View In Android",
+                                         "Create List View Android", 
+                                         "Android Example", 
+                                         "List View Source Code", 
+                                         "List View Array Adapter", 
+                                         "Android Example List View" 
+                                        };
+
+        
+        
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+          android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter); 
+        
+
+		
+		setContentView(R.layout.activity_inicio);
+	}
+	
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.inicio, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
